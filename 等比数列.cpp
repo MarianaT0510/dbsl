2f2a12656f86aed14075fa269ef53d465863663e
TZC <tzc051023@outlook.com>
2022-10-01 15:42:53 +0800

    添加项目文件。

---
+++ 等比数列.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <math.h>
+#pragma warning(disable : 4996)
+int main()
+{
+	int n;
+	float a1, q, An, Sn;
+	printf("请输入n:\n");
+	scanf("%d", &n);
+	if (n==1)
+	{
+		printf("请输入a1:\n");
+		scanf("%f", &a1); 
+		Sn = a1 *n;
+		printf("Sn=%f",Sn);
+	}
+	else
+	{
+		printf("请输入a1:");
+		scanf("%f", &a1);
+		printf("请输入q:");
+		scanf("%f", &q);
+		An = a1 * pow(q, n - 1);
+		Sn = (a1 - An * q) / (1 - q);
+		printf("An=%f,Sn=%f", An, Sn);
+	}
+}
